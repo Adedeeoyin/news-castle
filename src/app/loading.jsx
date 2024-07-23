5c4672fe6d4c1bc520ebb7f9c507445f64d55cc0
Iamdodo22 <asimiazeez493@gail.com>
2024-07-23 10:37:29 +0100

    changes

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,8 +2,8 @@
 const Loading = () => {
   return (
     <div className="flex flex-col w-full h-[60vh] items-center justify-center">
-      <div className="w-20 h-20 rounded-full border-y-4 border-t-choice1 border-b-choice2 animate-spin duration-300"></div>
-      <div className="text-choice1">loading...</div>
+      <div className="w-20 h-20 rounded-full border-y-4 border-t-choice1 dark:border-t-choice4 dark:border-b-choice4 border-b-choice2 animate-spin duration-300"></div>
+      <div className="text-choice1 dark:text-choice4">loading...</div>
     </div>
   )
 }
